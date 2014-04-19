47bcaa0fcb3f5c573db6800bb7a6c9c0fa85bac1
ChenChuang <daboshao@yeah.net>
2014-04-19 18:27:22 +0800

    recomm finish

--- recomm/test.py
+++ recomm/test.py
@@ -1,5 +1,6 @@
 import numpy as npy
 import scipy.sparse as spysp
+import scipy.sparse.linalg as spyalg
 import scipy.io
 import psycopg2 as pg
 import psycopg2.extras as pgextras
@@ -7,37 +8,19 @@ import psycopg2.extras as pgextras
 import datetime
 import random
 
+import sys, os
+mmpath = os.path.join(os.getcwd(), '..', 'mm')
+sys.path.insert(0, mmpath)
 import ccgraph as cg
+del sys.path[0]
 
-nr = 100
+nr = 49122
+nv = nr
+var = None
+varloc = None
+
 maxiter = 10
 tol = 1e-5
-
-def A():
-    info = {'nzero':0, 'nall':nr*nr}
-    amat = spysp.lil_matrix((nr, nr))
-    for i in npy.ndindex((nr,nr)):
-        c = random.random()
-        if c < 2:
-            amat[i] = c*100
-            info['nzero'] = info['nzero'] + 1
-    print "A done"
-    print info
-    return [amat, info]
-
-def b():
-    print "b done"
-    return npy.ones((nr, 1))
-
-def create_Ab_test():
-    [amat, info] = A()
-    return [amat, b(), info]
-
-def read_Ab_from_file(amatf, bmatf):
-    amat = read_mat(amatf)
-    bmat = read_mat(bmatf)
-    info = {'nzero':0, 'nall':nr*nr}
-    return [amat, bmat, info]
 
 def compute():
     x0 = None
@@ -50,14 +33,12 @@ def compute():
     wwt = WayWriter()
     wwt.write_profits(x)
 
-
-def compute_x(amat, bmat, x0):
+def compute_x(amat, bmat, x0, info=None):
     callback = Callback()
-    
     
     start_at = datetime.datetime.now()
     
-    x = spysp.linalg.gmres(amat, bmat, x0=x0, restart=None, xtype=None, M=None, restrt=None, 
+    x = spyalg.gmres(amat, bmat, x0=x0, restart=None, xtype=None, M=None, restrt=None, 
             tol = tol, 
             maxiter = maxiter, 
             callback = callback) 
@@ -77,98 +58,105 @@ class Callback:
         self.iters = self.iters + 1
 
 def cond(amat):
-    x = spysp.linalg.lsmr(amat, npy.ones((nr, 1)), damp=0.0, maxiter=None, show=True)
+    x = spysp.linalg.lsmr(amat, npy.ones((nv, 1)), damp=0.0, maxiter=None, show=True)
     return x
 
-def write_mat(amat, filename):
-    if not filename.endswith('.mtx'):
-        filename = filename + '.mtx'
-    scipy.io.mmwrite(filename, amat, comment='', field='real', precision=None)
-
-def read_mat(filename):
-    if not filename.endswith('.mtx'):
-        filename = filename + '.mtx'
-    return scipy.io.mmread(filename)
+def create_var_from_db():
+    global var
+    # var = range(1, nr+1)
+    var = range(1, 20)
+    global nv
+    nv = len(var)
+    global varloc
+    varloc = range(-1, nr)
 
 def fare(length):
-    return 10
+    return length
 
 def weight(d):
     return 1
+
+def isvar(wid):
+    return 1 <= wid <= nv
+    # return wid in var
 
 def create_Ab_from_db():
     def push_fs(wid, fs):
         i = wid - 1
         bmat[(i, 0)] = fs[0]
         x0mat[(i, 0)] = fs[0]
-        for j, f in enumerate(fs):
+        for j, f in enumerate(fs[1:]):
             if j == i:
-                f = 1 - f
-            if j > 0 and f > 0:
-                amat[(i, j-1)] = -f
+                f = f - 1
+            if f != 0:
+                amat[(i, j)] = -f
 
-    info = {'nzero':0, 'nall':nr*nr}
-    amat = spysp.lil_matrix((nr, nr))
-    bmat = npy.zeros((nr, 1))
-    x0mat = npy.zeros((nr, 1))
+    start_at = datetime.datetime.now() 
+
+    create_var_from_db()
+
+    info = {'nnz':0, 'nall':nv*nv}
+    amat = spysp.lil_matrix((nv, nv))
+    bmat = npy.zeros((nv, 1))
+    x0mat = npy.zeros((nv, 1))
     prd = PathReader()
     gw = cg.new_gw()
 
     alpha = 0.5
     beta = 0.5
 
-    for wid in xrange(1, nr+1):
-        fs = npy.zeros(nr+1)
-        for (swid, ewid, length, elonlats) in prd.paths_startwith_wid():
-            wsds = gw.find_wsds_within(elonlats, radius=10)
+    for i, wid in enumerate(var):
+        print i, wid, 
+        fs = npy.zeros(nv+1)
+        pcount = 0
+        for (swid, ewid, length, elonlats) in prd.paths_startwith_wid(wid):
+            pcount = pcount + 1
+            wsds = gw.find_wsds_within(elonlats, radius=2)
             for (near_wid, d) in wsds:
-                fs[near_wid] = fs[near_wid] + weight(d)
+                if isvar(near_wid):
+                    near_i = varloc[near_wid]
+                    fs[near_i] = fs[near_i] + weight(d)
             fs[0] = fs[0] + fare(length)
+        print "pcount:", pcount, 
         fs[0] = fs[0] * alpha
-        fs[1:] = fs[1:] * beta
-        fs[1:] = fs[1:] / sum(fs[1:])
+        fsum = sum(fs[1:])
+        if fsum > 0:
+            fs[1:] = fs[1:] / fsum
+            fs[1:] = fs[1:] * beta
         push_fs(wid, fs)
-        
+        print "done"
+    
+
+    end_at = datetime.datetime.now()
+    print "start:",start_at
+    print "end:",end_at
+    print "elapsed:",end_at - start_at
+ 
+    info['nnz'] = amat.nnz
     return [amat, bmat, x0mat, info]
 
-def create_exA_from_db():
-    def push_fs(wid, fs):
-        x0[wid, 0] = fs[0]
-        exa[wid, :] = fs
-
-    info = {'nzero':0, 'nall':nr*nr}
+# Ax = b
+# b + (I-A)x = x
+# 1   0     1   1
+# b (I-A) * x = x
+def create_exA_from_Ab(amat, bmat):
+    tnv = bmat.shape[0] + 1
+    info = {'nnz':0, 'nall':tnv*tnv}
+    exa = spysp.lil_matrix((tnv, tnv))
     
-    exa = spysp.lil_matrix((nr, nr))
-    exa[0,1:] = npy.zeros((1, nr))
+    x0mat = npy.ones((tnv, 1))
+    x0mat[1:, 0] = bmat[:,0]
+    
     exa[0,0] = 1
-
-    x0 = npy.zeros((nr, 1))
-    x0[0,0] = 1
-    
-    prd = PathReader()
-    gw = cg.new_gw()
-
-    alpha = 0.5
-    beta = 0.5
-
-    for wid in xrange(1, nr+1):
-        fs = npy.zeros(nr+1)
-        for (swid, ewid, length, elonlats) in prd.paths_startwith_wid():
-            wsds = gw.find_wsds_within(elonlats, radius=10)
-            for (near_wid, d) in wsds:
-                fs[near_wid] = fs[near_wid] + weight(d)
-            fs[0] = fs[0] + fare(length)
-        fs[0] = fs[0] * alpha
-        fs[1:] = fs[1:] * beta
-        fs[1:] = fs[1:] / sum(fs[1:])
-        push_fs(wid, fs)
-        
-    return [exa, x0, info]
-
-
+    exa[0,1:] = 0
+    exa[1:,0] = bmat
+    exa[1:,1:] = spysp.eye(tnv-1) - amat
+ 
+    info['nnz'] = amat.nnz
+    return [exa, x0mat, info] 
 
 class RemoteDB(object):
-    def __init__(self, database = "beijing_taxi"):
+    def __init__(self, database = "beijing_taxi_201211"):
         self.database = database
         self.user = "chenchuang"
         self.password = "123456"
@@ -189,7 +177,7 @@ class RemoteDB(object):
                     password = self.password,
                     host = self.host,
                     port = self.port)
-                    self.cursor = self.conn.cursor(cursor_factory = pgextras.DictCursor)
+            self.cursor = self.conn.cursor(cursor_factory = pgextras.DictCursor)
             return True
         except pg.DatabaseError, e:
             print e
@@ -203,21 +191,25 @@ class RemoteDB(object):
 
 
 class PathReader(RemoteDB):
-    def __init__(self, tbname = ""):
+    def __init__(self, tbname = "track_information_201211"):
         super(PathReader, self).__init__()
         self.tbname = tbname
 
-    def paths_startwith_wid(wid):
-        sql = "select id as pid, tid, tid_s_wid as swid, tid_e_wid as ewid, st_astext(tid_s) as s, st_astext(tid_e) as e, length as l, day from " + self.tbname + " where tid_s_wid = %s"
+    def paths_startwith_wid(self, wid):
+        sql = "select id as pid, tid, \
+            tid_s_lon as slon, \
+            tid_s_lat as slat, \
+            tid_s_wid as swid, \
+            tid_e_lon as elon, \
+            tid_e_lat as elat, \
+            tid_e_wid as ewid, tid_length as l, day from " + self.tbname + " where tid_s_wid = %s"
         self.cursor.execute(sql, (str(wid),))
         while True:            
             row = self.cursor.fetchone()
             if row is None:
                 break
-            estrs = row['e'][6:-1].split(',')
-            elonlats = (float(estrs[0]), float(estrs[1]))
+            elonlats = (row['elon'], row['elat'])
             yield [row['swid'], row['ewid'], row['l'], elonlats]
-
 
 class WayWriter(RemoteDB):
     def __init__(self, tbname = ""):
@@ -229,4 +221,51 @@ class WayWriter(RemoteDB):
             sql = "update " + self.tbname + " set (profit) = (%s) where wid = %s"
             self.cursor.execute(sql, (p, wid))
         self.conn.commit()
-        
+
+
+def write_mat(amat, filename):
+    if not filename.endswith('.mtx'):
+        filename = filename + '.mtx'
+    scipy.io.mmwrite(filename, amat, comment='', field='real', precision=None)
+
+def read_mat(filename):
+    if not filename.endswith('.mtx'):
+        filename = filename + '.mtx'
+    return scipy.io.mmread(filename).tolil()
+
+def read_Ab_from_file(amatf, bmatf):
+    amat = read_mat(amatf)
+    bmat = read_mat(bmatf)
+    info = {'nnz':amat.nnz, 'nall':nv*nv}
+    return [amat, bmat, info]
+
+
+def A():
+    info = {'nnz':0, 'nall':nv*nv}
+    amat = spysp.lil_matrix((nv, nv))
+    for i in npy.ndindex((nv,nv)):
+        c = random.random()
+        if c < 2:
+            amat[i] = c*100
+    print "A done"
+    info['nnz'] = amat.nnz
+    return [amat, info]
+
+def b():
+    print "b done"
+    return npy.ones((nv, 1))
+
+def create_Ab_test():
+    [amat, info] = A()
+    return [amat, b(), info]
+
+def print_mat(m, ur, dr, lc, rc):
+    for i in xrange(ur, dr):
+        for j in xrange(lc, rc):
+            if m[i,j] != 0:
+                print "{:5.2f}".format(m[i,j]), 
+            else:
+                print " "*5,
+        print ""
+
+
